de9bcd3ed6fac39d24a74c94ceb8258b3d28d4ed
Mark Brian <markbkiunga2017@gmail.com>
2024-07-11 18:33:05 +0300

    To continue from hoursWorkedOnDate function

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function createEmployeeRecord(Array) {
+  return {
+    firstName: Array[0],
+    familyName: Array[1],
+    title: Array[2],
+    payPerHour: Array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+// let testEmployee = createEmployeeRecord(['Gray', 'Worm', 'Security', 1]);
+// console.log(testEmployee);
+
+function createEmployeeRecords(Array) {
+  return Array.map(createEmployeeRecord);
+}
+// let twoRows = [['moe', 'sizlak', 'barkeep', 2]];
+// console.log(createEmployeeRecords(twoRows));
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+  let [day, time] = dateStamp.split(' ');
+  let timeInEvent = {
+    type: 'TimeIn',
+    hour: parseInt(time, 10),
+    date: day,
+  };
+  employeeRecord.timeInEvents.push(timeInEvent);
+  return employeeRecord;
+}
+function createTimeOutEvent(employeeRecord, dateStamp) {
+  let [day, time] = dateStamp.split(' ');
+  let timeOutEvent = {
+    type: 'TimeOut',
+    hour: parseInt(time, 10),
+    date: day,
+  };
+  employeeRecord.timeOutEvents.push(timeOutEvent);
+  return employeeRecord;
+}
+
+function hoursWorkedOnDate() {}
